b6d330fac4c580d2e6cb3493c706adf7b2c75c4a
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-06-28 09:34:12 -0700

    d git commit -m base:

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,22 @@
 import './Header.scss'
 import logo from '../../assets/images/micro_logo.png'
+import { NavLink } from 'react-router-dom';
 
 function Header() {
 
   return (
     <>
      <div className='nav'>
-      <div className='nav__logo-container'>
-        <img className='nav__logo' src={logo} alt="" />
-      </div>
-        <ul className='nav__links'>
-          <li className='nav__link'>Learn More</li>
-          <li className='nav__link'>Events</li>
-        </ul>
-     </div>
+    <div className='nav__logo-container'>
+      <img className='nav__logo' src={logo} alt="Logo" />
+    </div>
+    <ul className='nav__links'>
+      <li className='nav__link'>
+      Learn More
+      </li>
+      <li className='nav__link'>Events</li>
+    </ul>
+  </div>
     </>
   )
 }
--- src/pages/LearnMore/LearnMore.jsx
+++ src/pages/LearnMore/LearnMore.jsx
@@ -1,12 +1,51 @@
-import './LearnMore.scss'
+import './LearnMore.scss';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function LearnMore() {
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
+const LearnMore = () => {
+  const [prompt, setPrompt] = useState('');
+  const [generatedText, setGeneratedText] = useState('');
+  const [error, setError] = useState('');
+
+  const generateText = async () => {
+    try {
+      const response = await axios.post(`${baseUrl}/learnmore/prompt`, {
+        prompt: prompt
+      });
+
+      const data = response.data;
+      setGeneratedText(data.choices[0].message.content);
+    } catch (error) {
+      console.error('Error:', error);
+      setError('Error generating response');
+    }
+  };
 
   return (
-    <>
-     <p>I exist</p>
-    </>
-  )
-}
+    <div>
+      <h1>OpenAI API Demo</h1>
+      <textarea
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        placeholder="Enter your prompt here"
+        rows="4"
+        cols="50"
+      ></textarea>
+      <br />
+      <button onClick={generateText}>Generate</button>
+      {generatedText && (
+        <div>
+          <h3>Generated Text:</h3>
+          <p>{generatedText}</p>
+        </div>
+      )}
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
 
-export default LearnMore
+export default LearnMore;
+
+
